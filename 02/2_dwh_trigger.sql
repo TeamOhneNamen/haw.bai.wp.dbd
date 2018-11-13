12cdf581d1bec7830212917dc3af8cdd2c4bf283
Thorben <>
2018-11-13 08:18:40 +0100

    dwh laeuft

--- 02/2_dwh_trigger.sql
+++ 02/2_dwh_trigger.sql
@@ -1,17 +1,20 @@
 drop table Fakten CASCADE CONSTRAINTS;
 
 CREATE TABLE Fakten(
-	LieferantID NUMBER(10),
+	LieferantenID NUMBER(10),
 	TeilID NUMBER(10),
 	Klassifikation VARCHAR(1),
 	Jahr NUMBER(4),
 	Monat NUMBER(2),
 	Tag NUMBER(2),
-FOREIGN KEY(LieferantID) REFERENCES Lieferanten,
+FOREIGN KEY(LieferantenID) REFERENCES Lieferanten,
 FOREIGN KEY(TeilID) REFERENCES Teile);
 
 CREATE OR REPLACE TRIGGER DWH
 AFTER INSERT OR UPDATE ON Geliefert
+REFERENCING NEW AS NEW OLD AS OLD
+
+FOR EACH ROW
 declare 
 jahr    number(4);
 monat   number(2);
@@ -19,9 +22,10 @@ tag     number(2);
 klasse 	VARCHAR(1);
 begin
 	SELECT Klassifikation INTO Klasse from Teile WHERE TeilID = :new.TeilID;
-    extract(year from :new.Lieferdatum) into jahr;
-    extract(month from :new.Lieferdatum) into monat;
-    extract(day from :new.Lieferdatum) into tag;
-    Insert into Fakten values(:new.LieferantID, :new.TeilID, jahr, monat, tag);
+    jahr := extract(year from :new.Lieferdatum);
+    monat := extract(month from :new.Lieferdatum);
+    tag := extract(day from :new.Lieferdatum);
+    Insert into Fakten 
+    values(:new.LieferantenID, :new.TeilID, Klasse, jahr, monat, tag);
 end;
 	
